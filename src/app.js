0c5972684f0adeda38ae785b5be4f59e479c1752
Jordan Epps <jordanepps@gmail.com>
2019-03-27 07:57:10 -0400

    refactor with routes and ad post and delete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 
-const ArticlesService = require('./articles-service');
+const articlesRouter = require('./articles/Articles-router');
 
 const app = express();
 
@@ -15,28 +15,7 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-app.get('/articles', (req, res, next) => {
-	const knexInstance = req.app.get('db');
-	ArticlesService.getAllArticles(knexInstance)
-		.then(articles => {
-			res.json(articles);
-		})
-		.catch(next);
-});
-
-app.get('/articles/:article_id', (req, res, next) => {
-	const knexInstance = req.app.get('db');
-	ArticlesService.getById(knexInstance, req.params.article_id)
-		.then(article => {
-			if (!article) {
-				return res.status(404).json({
-					error: { message: `Article doesn't exist` }
-				});
-			}
-			res.json(article);
-		})
-		.catch(next);
-});
+app.use('/articles', articlesRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello, boilerplate!');
